9bdf92351b478c0a33ef78f6fdd028c119298555
egorioo <egorrudenko2002@gmail.com>
2021-10-21 17:43:06 +0300

    added comments

--- src/main/java/ua/edu/sumdu/j2se/RudenkoEgor/tasks/Task.java
+++ src/main/java/ua/edu/sumdu/j2se/RudenkoEgor/tasks/Task.java
@@ -10,12 +10,14 @@ public class Task {
     private boolean active;
     private boolean repeat;
 
+    /*Конструктор неактивной задачи которая выполняется один раз  */
     public Task(String title,int time) {
         this.title = title;
         this.time = time;
         this.repeat = false;
     }
 
+    /*Конструктор неактивной задачи, которая выполняется с интервалом  */
     public Task(String title, int start, int end, int interval) {
         this.title = title;
         this.start = start;
@@ -24,27 +26,34 @@ public class Task {
         this.repeat = true;
     }
 
+    /*Геттер для названия */
     public String getTitle() {
         return title;
     }
 
+    /*Сеттер для названия*/
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /*Метод для проверки состояния задачи */
     public boolean isActive() {
         return active;
     }
 
+    /*Метод для установления состояния задачи */
     public void setActive(boolean active){
         this.active = active;
     }
 
+    /*Геттер для времени. Возвращает время выполненения,
+    или время начала для задачи которая повторяется  */
     public int getTime(){
         if (repeat) return start;
         else return time;
     }
 
+    /*Сеттер для времени. Превращает повторяющуюся задачу в неповторяющуюся */
     public void setTime(int time){
         this.time = time;
         if (repeat) {
@@ -55,21 +64,25 @@ public class Task {
         }
     }
 
+    /*Геттер для времени повторения. Если не повторяется возвращается время выполнения */
     public int getStartTime(){
         if (repeat) return start;
         else return time;
     }
 
+    /*Геттер для конца выполнения повт. задачи. Если не повторяется возвращается время выполнения */
     public int getEndTime(){
         if (repeat) return end;
         else return time;
     }
 
+    /*Геттер для интервала. Если не повторяется возвращается 0*/
     public int getRepeatInterval(){
         if (repeat) return interval;
         else return 0;
     }
 
+    /*Сеттер для времени начала, конца и интервала. Если задача не повторяется, превращает в повт.*/
     public void setTime(int start, int end, int interval) {
         this.start = start;
         this.end = end;
@@ -80,11 +93,12 @@ public class Task {
         }
     }
 
+    /*Геттер для проверки состояния задачи. Повторяется или нет */
     public boolean isRepeated(){
         return repeat;
     }
 
-
+    /*Ищет следующую задачу*/
     public int nextTimeAfter(int current) {
         if (!active) return -1;
         if (!repeat) {
